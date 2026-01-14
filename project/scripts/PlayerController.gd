18e5c3ee1b102da0398f623bcd1dba0482271ce6
Alexey <rusinow.aleksey2016@yandex.ru>
2026-01-14 11:25:42 +0500

    reorganization + movement improvements

---
+++ project/scripts/PlayerController.gd
@@ -0,0 +1,38 @@
+extends Node3D
+
+#Variables
+
+var plooking = "forward"
+
+#var rotation_direction = Vector3()
+#var rotation_speed = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	match [plooking, ]:
+		
+		["forward", 1]:
+			plooking = "up"
+			rotate_x(PI / 2)
+			
+		["up", -1]:
+			plooking = "down"
+			rotate_x(PI / 2)
+		_:
+			pass
+	
+	#_get_input()
+	#transform.basis = Basis(Quaternion.from_euler(rotation_direction))
+	#global_rotate(rotation_direction, 0.1)
+
+# _get_input():
+	#rotation_direction.x = Input.get_axis("Down", "Up")
+
+	#rotation_direction.y = Input.get_axis("Right", "Left")
+	#print(rotation_direction)
