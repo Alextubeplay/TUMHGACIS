52964a2b0a81032eb03abb2567bcdfa263a50f21
Alexey <121627173+Alextubeplay@users.noreply.github.com>
2026-01-14 13:16:25 +0500

    Still not working

--- project/scripts/PlayerController.gd
+++ project/scripts/PlayerController.gd
@@ -3,8 +3,8 @@ extends Node3D
 #Variables
 
 var plooking = "forward"
+var rotation_direction = Vector3()
 
-#var rotation_direction = Vector3()
 #var rotation_speed = 5
 
 # Called when the node enters the scene tree for the first time.
@@ -15,17 +15,20 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	match [plooking, ]:
+	rotation_direction.x = Input.get_axis("Down", "Up")
+	
+	match [plooking, rotation_direction.x]:
 		
 		["forward", 1]:
 			plooking = "up"
 			rotate_x(PI / 2)
+			print("YAY")
 			
 		["up", -1]:
 			plooking = "down"
 			rotate_x(PI / 2)
-		_:
-			pass
+			print("non")
+	
 	
 	#_get_input()
 	#transform.basis = Basis(Quaternion.from_euler(rotation_direction))
