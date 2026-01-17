eb316d7d45cfb1184339cd482024c82cdeba6d90
Alexey <121627173+Alextubeplay@users.noreply.github.com>
2026-01-17 12:55:17 +0500

    bad

--- project/scripts/PlayerController.gd
+++ project/scripts/PlayerController.gd
@@ -11,75 +11,75 @@ func _process(delta):
 	match [plooking, Input.is_action_just_pressed("Left")]:
 		["forward", true]:
 			plooking = "left"
-			rotate_y(PI / 2)
+			rotation.y = (PI / 2)
 		
 		["up", true]:
 			plooking = "left"
-			rotate_x(-PI / 4)
-			rotate_y(PI / 2)
+			rotation.x = (0)
+			rotation.y = (PI / 2)
 		
 		["down", true]:
 			plooking = "left"
-			rotate_x(PI / 4)
-			rotate_y(PI / 2)
+			rotation.x = (0)
+			rotation.y = (PI / 2)
 		
 		["right", true]:
 			plooking = "forward"
-			rotate_y(PI / 2)
+			rotation.y = (0)
 	
 	match [plooking, Input.is_action_just_pressed("Right")]:
 		["forward", true]:
 			plooking = "right"
-			rotate_y(-PI / 2)
+			rotation.y = (-PI / 2)
 		
 		["up", true]:
 			plooking = "right"
-			rotate_x(-PI / 4)
-			rotate_y(-PI / 2)
+			rotation.x = (0)
+			rotation.y = (-PI / 2)
 		
 		["down", true]:
 			plooking = "right"
-			rotate_x(PI / 4)
-			rotate_y(-PI / 2)
+			rotation.x = (0)
+			rotation.y = (-PI / 2)
 		
 		["left", true]:
 			plooking = "forward"
-			rotate_y(-PI / 2)
+			rotation.y = (0)
 	
 	match [plooking, Input.is_action_just_pressed("Up")]:
 		["forward", true]:
 			plooking = "up"
-			rotate_x(PI / 4)
+			rotation.x = (PI / 4)
 		
 		["right", true]:
 			plooking = "up"
-			rotate_y(PI / 2)
-			rotate_x(PI / 4)
+			rotation.y = (0)
+			rotation.x = (PI / 4)
 		
 		["down", true]:
 			plooking = "forward"
-			rotate_x(PI / 4)
+			rotation.x = (0)
 		
 		["left", true]:
 			plooking = "up"
-			rotate_y(-PI / 2)
-			rotate_x(PI / 4)
+			rotation.y = (0)
+			rotation.x = (PI / 4)
 	
 	match [plooking, Input.is_action_just_pressed("Down")]:
 		["forward", true]:
 			plooking = "down"
-			rotate_x(-PI / 4)
+			rotation.x = (-PI / 4)
 		
 		["right", true]:
 			plooking = "down"
-			rotate_y(PI / 2)
-			rotate_x(-PI / 4)
+			rotation.y = (0)
+			rotation.x = (-PI / 4)
 		
 		["up", true]:
 			plooking = "forward"
-			rotate_x(-PI / 4)
+			rotation.x = (0)
 		
 		["left", true]:
 			plooking = "down"
-			rotate_y(-PI / 2)
-			rotate_x(-PI / 4)
+			rotation.y = (0)
+			rotation.x = (-PI / 4)
