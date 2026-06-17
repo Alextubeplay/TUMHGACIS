2ec25fbc0ffae8d670b4da70a9215da7fad47256
Alexey <rusinow.aleksey2016@yandex.ru>
2026-06-17 23:15:22 +0500

    Credits menu

--- project/scripts/Main_menu.gd
+++ project/scripts/Main_menu.gd
@@ -3,15 +3,18 @@ extends Node3D
 @onready var main_menu_container = $Main_menu/Menu
 @onready var difficulty_menu_container = $Main_menu/Difficulty_settings
 @onready var how_to_play_menu_container = $Main_menu/How_to_play_menu
+@onready var credits_menu_container = $Main_menu/Credits_menu
 
 func _ready() -> void:
 	main_menu_container.show()
 	difficulty_menu_container.hide()
 	how_to_play_menu_container.hide()
+	credits_menu_container.hide()
 	
 	main_menu_container.get_node("Play").pressed.connect(_on_play_pressed)
 	main_menu_container.get_node("Difficulty").pressed.connect(_on_difficulty_pressed)
 	main_menu_container.get_node("How_to_play").pressed.connect(_on_how_to_play_pressed)
+	main_menu_container.get_node("Credits").pressed.connect(_on_credits_pressed)
 	main_menu_container.get_node("Exit").pressed.connect(_on_exit_pressed)
 	
 	difficulty_menu_container.get_node("HBoxContainer/1").pressed.connect(_on_difficulty_selected.bind(1))
@@ -20,6 +23,7 @@ func _ready() -> void:
 	difficulty_menu_container.get_node("Back").pressed.connect(_on_back_pressed)
 	
 	how_to_play_menu_container.get_node("Back").pressed.connect(_on_how_to_play_back_pressed)
+	credits_menu_container.get_node("Back").pressed.connect(_on_credits_back_pressed)
 	
 	_update_difficulty_colors()
 
@@ -56,14 +60,26 @@ func _on_how_to_play_back_pressed() -> void:
 	how_to_play_menu_container.hide()
 	main_menu_container.show()
 
+func _on_credits_pressed() -> void:
+	main_menu_container.hide()
+	credits_menu_container.show()
+
+func _on_credits_back_pressed() -> void:
+	credits_menu_container.hide()
+	main_menu_container.show()
+
 func _update_difficulty_colors() -> void:
 	if not ShiftSettings:
 		return
 	var current_diff = ShiftSettings.difficulty
+	
+	var active_color = Color.from_hsv(0.12, 0.8, 1.0)
+	var inactive_color = Color.from_hsv(0.12, 0.2, 0.4)
+	
 	for i in range(1, 4):
 		var btn = difficulty_menu_container.get_node("HBoxContainer/" + str(i))
 		if btn:
-			var color = Color.WHITE if i == current_diff else Color(0.466, 0.466, 0.466, 1.0)
+			var color = active_color if i == current_diff else inactive_color
 			btn.add_theme_color_override("font_color", color)
 			btn.add_theme_color_override("font_hover_color", color)
 			btn.add_theme_color_override("font_pressed_color", color)
