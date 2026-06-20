0f75cfbe97c9b7631b0f6633891f87582c8ab1e6
Alexey <rusinow.aleksey2016@yandex.ru>
2026-06-20 23:10:20 +0500

    transit

--- project/scripts/Main_menu.gd
+++ project/scripts/Main_menu.gd
@@ -4,15 +4,18 @@ extends Node3D
 @onready var difficulty_menu_container = $Main_menu/Difficulty_settings
 @onready var how_to_play_menu_container = $Main_menu/How_to_play_menu
 @onready var credits_menu_container = $Main_menu/Credits_menu
+@onready var settings_menu_container = $Main_menu/Game_settings
 
 func _ready() -> void:
 	main_menu_container.show()
 	difficulty_menu_container.hide()
 	how_to_play_menu_container.hide()
 	credits_menu_container.hide()
+	settings_menu_container.hide()
 	
 	main_menu_container.get_node("Play").pressed.connect(_on_play_pressed)
 	main_menu_container.get_node("Difficulty").pressed.connect(_on_difficulty_pressed)
+	main_menu_container.get_node("Settings").pressed.connect(_on_settings_pressed)
 	main_menu_container.get_node("How_to_play").pressed.connect(_on_how_to_play_pressed)
 	main_menu_container.get_node("Credits").pressed.connect(_on_credits_pressed)
 	main_menu_container.get_node("Exit").pressed.connect(_on_exit_pressed)
@@ -22,10 +25,12 @@ func _ready() -> void:
 	difficulty_menu_container.get_node("HBoxContainer/3").pressed.connect(_on_difficulty_selected.bind(3))
 	difficulty_menu_container.get_node("Back").pressed.connect(_on_back_pressed)
 	
-	how_to_play_menu_container.get_node("Back").pressed.connect(_on_how_to_play_back_pressed)
-	credits_menu_container.get_node("Back").pressed.connect(_on_credits_back_pressed)
-	
-	_update_difficulty_colors()
+	if how_to_play_menu_container.has_node("Back"):
+		how_to_play_menu_container.get_node("Back").pressed.connect(_on_how_to_play_back_pressed)
+	if credits_menu_container.has_node("Back"):
+		credits_menu_container.get_node("Back").pressed.connect(_on_credits_back_pressed)
+	if settings_menu_container.has_node("Back"):
+		settings_menu_container.get_node("Back").pressed.connect(_on_settings_back_pressed)
 
 func _on_play_pressed() -> void:
 	if ShiftSettings:
@@ -38,6 +43,14 @@ func _on_difficulty_pressed() -> void:
 	main_menu_container.hide()
 	difficulty_menu_container.show()
 	_update_difficulty_colors()
+
+func _on_settings_pressed() -> void:
+	main_menu_container.hide()
+	settings_menu_container.show()
+
+func _on_settings_back_pressed() -> void:
+	settings_menu_container.hide()
+	main_menu_container.show()
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
@@ -74,13 +87,12 @@ func _update_difficulty_colors() -> void:
 	var current_diff = ShiftSettings.difficulty
 	
 	var active_color = Color.from_hsv(0.12, 0.8, 1.0)
-	var inactive_color = Color.from_hsv(0.12, 0.2, 0.4)
+	var inactive_color = Color.from_hsv(0.12, 0.2, 0.5)
 	
-	for i in range(1, 4):
-		var btn = difficulty_menu_container.get_node("HBoxContainer/" + str(i))
-		if btn:
-			var color = active_color if i == current_diff else inactive_color
-			btn.add_theme_color_override("font_color", color)
-			btn.add_theme_color_override("font_hover_color", color)
-			btn.add_theme_color_override("font_pressed_color", color)
-			btn.add_theme_color_override("font_focus_color", color)
+	var btn1 = difficulty_menu_container.get_node("HBoxContainer/1")
+	var btn2 = difficulty_menu_container.get_node("HBoxContainer/2")
+	var btn3 = difficulty_menu_container.get_node("HBoxContainer/3")
+	
+	if btn1: btn1.modulate = active_color if current_diff == 1 else inactive_color
+	if btn2: btn2.modulate = active_color if current_diff == 2 else inactive_color
+	if btn3: btn3.modulate = active_color if current_diff == 3 else inactive_color
--- project/scripts/Pause_menu.gd
+++ project/scripts/Pause_menu.gd
@@ -2,9 +2,11 @@ extends CanvasLayer
 
 @export var player: Node3D
 
+@onready var pause_menu_container = $Menu
 @onready var resume_button: Button = $Menu/Continue
 @onready var settings_button: Button = $Menu/Settings
 @onready var exit_button: Button = $Menu/Exit
+@onready var settings_menu_container = $Game_settings
 
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
@@ -16,6 +18,11 @@ func _ready() -> void:
 	if resume_button: resume_button.pressed.connect(resume_game)
 	if settings_button: settings_button.pressed.connect(_on_settings_pressed)
 	if exit_button: exit_button.pressed.connect(_on_exit_pressed)
+	
+	if settings_menu_container:
+		settings_menu_container.hide()
+		if settings_menu_container.has_node("Back"):
+			settings_menu_container.get_node("Back").pressed.connect(_on_settings_back_pressed)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Escape"):
@@ -27,6 +34,9 @@ func _process(delta: float) -> void:
 func pause_game() -> void:
 	get_tree().paused = true
 	show()
+	pause_menu_container.show()
+	if settings_menu_container:
+		settings_menu_container.hide()
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func resume_game() -> void:
@@ -35,7 +45,14 @@ func resume_game() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _on_settings_pressed() -> void:
-	pass
+	pause_menu_container.hide()
+	if settings_menu_container:
+		settings_menu_container.show()
+
+func _on_settings_back_pressed() -> void:
+	if settings_menu_container:
+		settings_menu_container.hide()
+	pause_menu_container.show()
 
 func _on_exit_pressed() -> void:
 	get_tree().paused = false
